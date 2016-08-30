1f5977f194bc48dfa68fd884bf9ed8f77ed804a8
Steve Bradley <stevebradley@gmail.com>
2016-08-30 01:55:06 -0400

    Add Shortest Unique Prefix

---
+++ src/interviewbit/shortest_unique_prefix.py
@@ -0,0 +1,30 @@
+#https://www.interviewbit.com/problems/shortest-unique-prefix/
+class Trie:
+    def __init__(self, children={}):
+        self.children = children
+
+    def __str__(self):
+        return str(self.children)
+
+    def add(self, word):
+        if word:
+            if word[0] not in self.children:
+                self.children[word[0]] = Trie({})
+            self.children[word[0]].add(word[1:])
+
+def shortest_prefix(t, word, pfx):
+    if word:
+        if len(t.children.keys()) > 1:
+            return (pfx + word[0] + shortest_prefix(t.children[word[0]], word[1:], ''))
+        else:
+            return(shortest_prefix(t.children[word[0]], word[1:], pfx + word[0]))
+    else:
+        return('')
+
+words = ["zebra", "dogs", "duck", "dove", "doggie", "zenbot"]
+tree = Trie({})
+for word in words:
+    tree.add(word)
+s = shortest_prefix(tree, words[5], "")
+print(s)
+
